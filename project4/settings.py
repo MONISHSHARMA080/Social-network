ee394517b9e1b5efd3a2a0b59cf23fb1caa0b55d
Monish Sharma <107314033+MONISHSHARMA080@users.noreply.github.com>
2023-10-28 16:46:31 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -136,8 +136,12 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gbEFB6ca26cDeb2dbfF3f3eAd3FFgbea',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '53461',
     }
 }
 
