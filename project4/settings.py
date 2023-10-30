3e9a872e1856bca4c98bd8e069b8ee75141c92c3
unknown <monishsharma080@gmail.com>
2023-10-30 23:01:03 +0530

    added host and cors origin in settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://social-network-monish.onrender.com']
 
 
 # Application definition
@@ -96,7 +96,7 @@ SIMPLE_JWT = SIMPLE_JWT = {
 
 
 # CORS_ALLOWED_ORIGINS = ['https://network-sigma.vercel.app','http://localhost:5173']
-CORS_ALLOWED_ORIGINS = ['https://network-sigma.vercel.app','http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['https://network-sigma.vercel.app']
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
